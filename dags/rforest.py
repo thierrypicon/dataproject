20fe9e148eeb47eed38a0b2f8e2e68f55fb230fe
thierry <thierry.picon@yvantitiov.com>
2020-09-24 11:14:51 +0200

    lastversion

--- dags/rforest.py
+++ dags/rforest.py
@@ -6,45 +6,58 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestRegressor
 from sklearn import metrics
 import pickle
+from airflow.utils.dates import days_ago
+
+from airflow.models import DAG
+from airflow.operators.python_operator import PythonOperator
+
+
 
 '''
 The idea is to wrap the following in a function (a python callable), 
 that will be called by an Airflow Python Operator.
 '''
 
+
+def traitement():
+
 # This filepath should be passed as an argument
-dataset_filepath = '/home/yvantitiov/dataproject/petrol_consumption.csv'
+	dataset_filepath = '/home/yvantitiov/dataproject/petrol_consumption.csv'	
 
-dataset = pd.read_csv(dataset_filepath)
+	dataset = pd.read_csv(dataset_filepath)
 
-attributes = dataset.iloc[:, 0:4].values
-labels = dataset.iloc[:, 4].values
+	attributes = dataset.iloc[:, 0:4].values
+	labels = dataset.iloc[:, 4].values
 
 # Divide the dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(
-    attributes, labels, test_size=0.2, random_state=0
-)
+	X_train, X_test, y_train, y_test = train_test_split(
+    	attributes, labels, test_size=0.2, random_state=0
+	)
 
 # Feature Scaling
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
+	sc = StandardScaler()
+	X_train = sc.fit_transform(X_train)
+	X_test = sc.transform(X_test)
 
 # Train the model
-regressor = RandomForestRegressor(n_estimators=200, random_state=0)
-regressor.fit(X_train, y_train)
-y_pred = regressor.predict(X_test)
+	regressor = RandomForestRegressor(n_estimators=200, random_state=0)
+	regressor.fit(X_train, y_train)
+	y_pred = regressor.predict(X_test)
 
 # Evaluate the performance of the model
-print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
-print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
-print('Root Mean Squared Error:', np.sqrt(
-    metrics.mean_squared_error(y_test, y_pred)))
+	print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
+	print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
+	print('Root Mean Squared Error:', np.sqrt(
+    		metrics.mean_squared_error(y_test, y_pred)))
 
 # Save the model to disk
 # This filepath should be passed as an argument
-filename = 'regressor.pickle'
+	filename = 'regressor.pickle'
 
-pickle.dump(regressor, open(filename, 'wb'))
-print("Model saved.")
+	pickle.dump(regressor, open(filename, 'wb'))
+	print("Model saved.")
+	return True
+
+
+traitement()
 
